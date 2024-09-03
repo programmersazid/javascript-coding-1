0dbce18c66a752036a02b7f1ae9492532fb9afaf
modabbir <modabbir276@gmail.com>
2024-09-04 00:41:47 +0600

    Subject marking Average

---
+++ average.js
@@ -0,0 +1,12 @@
+var Mathmetics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+
+var sum = (Mathmetics + Biology + Chemistry + Physics + Bangla);
+sum = parseFloat(sum);
+ 
+var average = sum/5;
+average = parseFloat(average);
+console.log(average.toFixed(2));
